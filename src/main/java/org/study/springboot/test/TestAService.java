0605a771f066f1cad7732be939d4e234fa4ae685
lar542 <lar542@unvus.com>
2024-02-18 21:07:51 +0900

    DI 방식별 순환 참조 에러 테스트 순서대로 생성자 주입, 필드 주입, Setter 주입 방식 순환 참조 허용 여부는 application.properties 에서 할 수 있다. 다만, 생성자 주입 방식의 경우는 순환 참조 허용 설정을 해도 컴파일 에러를 피할 수 없다.

---
+++ src/main/java/org/study/springboot/test/TestAService.java
@@ -0,0 +1,21 @@
+package org.study.springboot.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestAService {
+
+    /*private final TestBService bService;
+
+    public TestAService(TestBService bService) {
+        this.bService = bService;
+    }*/
+    /*@Autowired
+    private TestBService bService;*/
+    /*private TestBService bService;
+    @Autowired
+    public void setbService(TestBService bService) {
+        this.bService = bService;
+    }*/
+}
---
+++ src/main/java/org/study/springboot/test/TestBService.java
@@ -0,0 +1,22 @@
+package org.study.springboot.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestBService {
+
+    /*private final TestAService aService;
+
+    public TestBService(TestAService aService) {
+        this.aService = aService;
+    }*/
+    /*@Autowired
+    private TestAService aService;*/
+    /*private TestAService aService;
+
+    @Autowired
+    public void setaService(TestAService aService) {
+        this.aService = aService;
+    }*/
+}
